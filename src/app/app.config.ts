b7bff7b2ce22366ef897314271aeb0be7b70257c
Kevin Müller <kevin.mueller@tuta.com>
2024-05-29 20:06:21 +0200

    first setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,26 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { Environment } from './environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes), 
+    provideFirebaseApp(() => initializeApp({
+      "projectId":"da-bubble-e6d79",
+      "appId":"1:728652156728:web:52fda9767640c5a89efbcd",
+      "storageBucket":"da-bubble-e6d79.appspot.com",
+      "apiKey":Environment.API_KEY,
+      "authDomain":"da-bubble-e6d79.firebaseapp.com",
+      "messagingSenderId":"728652156728"})), 
+      provideFirestore(() => getFirestore()), 
+      provideStorage(() => getStorage()),
+      provideAuth(() => getAuth()), provideAnimationsAsync()
+    ]
 };
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,3 @@
+export enum Environment {
+    API_KEY = 'AIzaSyD0sO7LDB0dsLfdQlV_Ya3NQj_MW2dEy2U'
+}
