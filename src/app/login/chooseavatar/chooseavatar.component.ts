7d29566511b3504c9e270bc2e9684679ffefd79e
Marcel Lakotta <marcel.lakotta@aol.de>
2024-05-30 09:43:27 +0200

    login components added

---
+++ src/app/login/chooseavatar/chooseavatar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chooseavatar',
+  standalone: true,
+  imports: [],
+  templateUrl: './chooseavatar.component.html',
+  styleUrl: './chooseavatar.component.scss'
+})
+export class ChooseavatarComponent {
+
+}
