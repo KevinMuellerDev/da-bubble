1187c7f9fa8bb796f17c4ded7b695e8a37789b4e
Kevin Müller <kevin.mueller@tuta.com>
2024-05-30 10:36:54 +0200

    display bugfix

--- src/app/mainsection/mainsection.component.ts
+++ src/app/mainsection/mainsection.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
+
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ChannelComponent } from './channel/channel.component';
 import { ThreadComponent } from './thread/thread.component';
 import { CommonModule } from '@angular/common';
-import { MatIconModule } from '@angular/material/icon';
+import { HeaderComponent } from '../shared/components/header/header.component';
 
 @Component({
   selector: 'app-mainsection',
   standalone: true,
-  imports: [CommonModule, SidebarComponent, ChannelComponent, ThreadComponent, MatIconModule],
+  imports: [CommonModule, HeaderComponent, SidebarComponent, ChannelComponent, ThreadComponent],
   templateUrl: './mainsection.component.html',
   styleUrl: './mainsection.component.scss'
 })
