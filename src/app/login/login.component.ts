f90b4130302f6bf63ce2e53d25e245002f18493d
Marcel Lakotta <marcel.lakotta@aol.de>
2024-05-30 10:08:37 +0200

    edit login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,12 +4,13 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [RouterLink, CommonModule, MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
