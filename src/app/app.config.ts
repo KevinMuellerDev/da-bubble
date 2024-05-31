fc92dbc6d2fa40c9861f160576767ec24ccb870a
forstmeister <c.foerster@tutanota.com>
2024-05-31 11:20:28 +0200

    implemented open/close funktion for Channels & DMs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,21 +7,26 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { Environment } from '../../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes), 
-    provideFirebaseApp(() => initializeApp({
-      "projectId":"da-bubble-e6d79",
-      "appId":"1:728652156728:web:52fda9767640c5a89efbcd",
-      "storageBucket":"da-bubble-e6d79.appspot.com",
-      "apiKey":Environment.API_KEY,
-      "authDomain":"da-bubble-e6d79.firebaseapp.com",
-      "messagingSenderId":"728652156728"})), 
-      provideFirestore(() => getFirestore()), 
-      provideStorage(() => getStorage()),
-      provideAuth(() => getAuth()), provideAnimationsAsync()
-    ]
+    provideRouter(routes),
+    provideAnimations(),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'da-bubble-e6d79',
+        appId: '1:728652156728:web:52fda9767640c5a89efbcd',
+        storageBucket: 'da-bubble-e6d79.appspot.com',
+        apiKey: Environment.API_KEY,
+        authDomain: 'da-bubble-e6d79.firebaseapp.com',
+        messagingSenderId: '728652156728',
+      })
+    ),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
+    provideAuth(() => getAuth()),
+    provideAnimationsAsync(),
+  ],
 };
--- src/app/mainsection/sidebar/sidebar.component.ts
+++ src/app/mainsection/sidebar/sidebar.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
+  animations: [
+    trigger('openClose', [
+      state('closed', style({ display: 'none' })),
+      state('open', style({ display: 'flex' })),
+    ]),
+  ],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
+  menuStates: { [state: string]: 'open' | 'closed' } = {
+    channel: 'closed',
+    message: 'closed',
+  };
 
+  toggleMenu(menu: 'channel' | 'message') {
+    this.menuStates[menu] =
+      this.menuStates[menu] === 'open' ? 'closed' : 'open';
+  }
 }
