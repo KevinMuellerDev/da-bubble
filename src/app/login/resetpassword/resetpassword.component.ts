2e9b4f8d494c05699d80888e147db47312f3e0a3
Marcel Lakotta <marcel.lakotta@aol.de>
2024-05-31 07:14:09 +0200

    style resetpassword area

--- src/app/login/resetpassword/resetpassword.component.ts
+++ src/app/login/resetpassword/resetpassword.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-resetpassword',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './resetpassword.component.html',
   styleUrl: './resetpassword.component.scss'
 })
