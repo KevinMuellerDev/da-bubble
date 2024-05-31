327421ec2ebd92338542fffd28cbddd179ec7ea2
Marcel Lakotta <marcel.lakotta@aol.de>
2024-05-31 09:18:41 +0200

    start style imprint & privacy policy, edit intro animation

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
