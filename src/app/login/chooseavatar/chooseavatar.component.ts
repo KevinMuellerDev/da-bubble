136164cda0f9603392b4939daba19f09f5bcdb55
Marcel Lakotta <marcel.lakotta@aol.de>
2024-06-02 10:24:02 +0200

    edited chooseAvatar via the registration
    The standard avatars are first defined in storage.service.ts.

--- src/app/login/chooseavatar/chooseavatar.component.ts
+++ src/app/login/chooseavatar/chooseavatar.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { StorageService } from '../../shared/services/storage.service';
 
 @Component({
   selector: 'app-chooseavatar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './chooseavatar.component.html',
-  styleUrl: './chooseavatar.component.scss'
+  styleUrls: ['./chooseavatar.component.scss']
 })
-export class ChooseavatarComponent {
+export class ChooseavatarComponent implements OnInit {
+  avatars: string[] = [];
+  selectedAvatar: string = '../../assets/img/login/default_profil_img.png'; // default img
 
+  constructor(private storageService: StorageService) { }
+
+  ngOnInit() {
+    this.avatars = this.storageService.getAvatars();
+  }
+
+  selectAvatar(avatar: string) {
+    this.selectedAvatar = avatar;
+  }
 }
--- src/app/shared/services/storage.service.ts
+++ src/app/shared/services/storage.service.ts
@@ -5,5 +5,18 @@ import { Injectable } from '@angular/core';
 })
 export class StorageService {
 
+  private avatars: string[] = [
+    '../assets/img/profile/elias_neumann.png',
+    '../assets/img/profile/elise_roth.png',
+    '../assets/img/profile/frederik_beck.png',
+    '../assets/img/profile/noah_braun.png',
+    '../assets/img/profile/sofia_müller.png',
+    '../assets/img/profile/steffen_hoffmann.png'
+  ];
+
   constructor() { }
+
+  getAvatars(): string[] {
+    return this.avatars;
+  }
 }
