ca3593baab2dfc1308c318ef90bf3edda00246ae
Kevin Müller <kevin.mueller@tuta.com>
2024-06-02 16:13:07 +0200

    added storage.service

--- src/app/shared/services/storage.service.ts
+++ src/app/shared/services/storage.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Storage, ref, uploadBytesResumable, getDownloadURL } from '@angular/fire/storage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StorageService {
+  private readonly storage: Storage = inject(Storage);
+
+  files!: FileList;
+  fileName!: string | undefined;
 
   private avatars: string[] = [
     '../assets/img/profile/elias_neumann.png',
@@ -19,4 +24,41 @@ export class StorageService {
   getAvatars(): string[] {
     return this.avatars;
   }
+
+  
+  onFileSelected(input: HTMLInputElement) {
+    if (input.files?.item(0)?.size! > 1048576) {
+      this.fileName = "This file exceeds the size of 1024kb !"
+      return
+    }
+    if (!input.files || (input.files && !this.isValid(input))) return
+    this.files = input.files;
+    this.fileName = this.files.item(0)?.name
+  }
+
+
+  isValid(input: HTMLInputElement) {
+    let dataType = input.files?.item(0)?.type
+    dataType = dataType?.split('/').pop();
+    console.log(input.files?.item(0)?.size!);
+
+    return (dataType === 'jpeg' || dataType === 'jpg' || dataType === 'png' || dataType === 'gif')
+  }
+
+
+  async uploadFile() {
+    if (this.files == undefined) return
+    for (let i = 0; i < this.files.length; i++) {
+      const file = this.files.item(i);
+      if (file) {
+        const storageRef = ref(this.storage, 'TODO: IMPLEMENT ME');
+        await uploadBytesResumable(storageRef, file);
+        await getDownloadURL(storageRef).then((url) => {
+          //TODO: IMPLEMENT LOGIC
+        })
+      }
+    }
+  }
+
+
 }
