cfaff99ba881447ad85d99d0e831ed62b9d6ef52
forstmeister <c.foerster@tutanota.com>
2024-06-03 14:14:27 +0200

    added JSdoc to sidebar, styled head for the thread section

--- src/app/mainsection/sidebar/sidebar.component.ts
+++ src/app/mainsection/sidebar/sidebar.component.ts
@@ -9,7 +9,6 @@ import {
 import { CommonModule } from '@angular/common';
 import { UserService } from '../../shared/services/user.service';
 
-
 @Component({
   selector: 'app-sidebar',
   standalone: true,
@@ -23,28 +22,41 @@ import { UserService } from '../../shared/services/user.service';
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
-
 export class SidebarComponent {
+  /**
+   * An object representing the states of different menus.
+   * Each menu state can either be 'open' or 'closed'.
+   *
+   * @type {{ [state: string]: 'open' | 'closed' }}
+   */
+
   menuStates: { [state: string]: 'open' | 'closed' } = {
     channel: 'closed',
     message: 'closed',
   };
   userService: UserService = inject(UserService);
 
-
+  /**
+   * Toggles the state of the specified menu between 'open' and 'closed'.
+   *
+   * @param {'channel' | 'message'} menu - The menu to be toggled. It can either be 'channel' or 'message'.
+   */
   toggleMenu(menu: 'channel' | 'message') {
     this.menuStates[menu] =
       this.menuStates[menu] === 'open' ? 'closed' : 'open';
   }
 
   /**
-  * Function returns the class of user status for online indicator div
-  * 
-  * @param type string - to determine which value should be returned
-  * @returns class as a string
-  */
+   * Function returns the class of user status for online indicator div
+   *
+   * @param type string - to determine which value should be returned
+   * @returns class as a string
+   */
   getUserStatus() {
-    const loggedIn = this.userService.userInfo.isLoggedIn == true ? "online-div" : "offline-div";
-    return loggedIn
+    const loggedIn =
+      this.userService.userInfo.isLoggedIn == true
+        ? 'online-div'
+        : 'offline-div';
+    return loggedIn;
   }
 }
