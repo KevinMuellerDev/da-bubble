06111402046817ca7ada41c7eed913be8d0074f0
Kevin Mueller <kevin.mueller@tuta.com>
2024-06-06 08:57:01 +0200

    added check if user is verified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -81,7 +81,7 @@ export class LoginComponent implements OnInit {
         const user = userCredential.user;
         console.log('user.uid:', user.uid, user);
         sessionStorage.setItem("uid", user.uid); 
-        this.router.navigate(['/mainsection/' + user.uid]);
+        user.emailVerified ? this.router.navigate(['/mainsection/' + user.uid]) : console.warn('user not verified');
         this.isFormSubmitted = false;
       } catch (error) {
         console.error(error);
