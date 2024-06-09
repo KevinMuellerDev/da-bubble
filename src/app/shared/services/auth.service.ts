83d626b5c261577a49d7204c32538e3490a57766
Kevin Mueller <kevin.mueller@tuta.com>
2024-06-09 15:47:07 +0200

    added JSDOC to auth.service

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -10,9 +10,14 @@ import { confirmPasswordReset, signInWithEmailAndPassword } from '@angular/fire/
 export class AuthService {
   firestore: Firestore = inject(Firestore);
   verified: boolean = false;
+
   constructor(private userService: UserService) { }
 
-  
+  /**
+   * Function to login into firebase auth
+   * 
+   * @param key string - key to login to firebase auth
+   */
   async verifyChange(key: string) {
     const auth = getAuth();
     console.log(this.userService.userInfo.email);
@@ -21,14 +26,18 @@ export class AuthService {
         console.log('succesfull');
         
       }).catch((error) => {
-        console.error(error);
-
+        console.error(error.code, error.message);
       });
 
 
   }
 
 
+  /**
+   * Update email of the user in firebase auth
+   * 
+   * @param mail string - prvoided email
+   */
   async updateUserMail(mail: string) {
     const auth = getAuth();
     updateEmail(auth.currentUser as User, mail).then(() => {
@@ -39,6 +48,12 @@ export class AuthService {
   }
 
 
+  /**
+   * Update Password of the user who forogt his password
+   * 
+   * @param code string - oobCode of the url
+   * @param key string - changed password as a string
+   */
   async confirmNewPassword(code:string ,key:string){
     const auth = getAuth();
 ;
