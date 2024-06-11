ad230edf44e5ad3f6169495c857f71c0433ba9aa
Kevin Mueller <kevin.mueller@tuta.com>
2024-06-11 10:08:24 +0200

    show users in menu when searching for them

--- src/app/mainsection/sidebar/add-new-user-to-channel/add-new-user-to-channel.component.ts
+++ src/app/mainsection/sidebar/add-new-user-to-channel/add-new-user-to-channel.component.ts
@@ -1,23 +1,29 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, ViewChild, ViewEncapsulation, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
 import { ChannelService } from '../../../shared/services/channel.service';
 import { UserService } from '../../../shared/services/user.service';
+import { user } from '@angular/fire/auth';
+import {MatMenuModule, MatMenuTrigger} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-add-new-user-to-channel',
   standalone: true,
-  imports: [MatDialogModule, FormsModule, CommonModule],
+  imports: [MatDialogModule, MatMenuModule, MatButtonModule, FormsModule, CommonModule],
   templateUrl: './add-new-user-to-channel.component.html',
-  styleUrl: './add-new-user-to-channel.component.scss'
+  styleUrl: './add-new-user-to-channel.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class AddNewUserToChannelComponent {
+  @ViewChild("specificUserInput") specificUserInput?: { nativeElement: { focus: () => void; }; };
   channelService: ChannelService = inject(ChannelService);
   userService: UserService = inject(UserService);
 
   specificUser: boolean;
-  userList: object[] = [];
+  userList: any[] = [];
   inputs = {
     specificUser: ''
   }
@@ -37,17 +43,21 @@ export class AddNewUserToChannelComponent {
 
 
   searchUser() {
+    this.userList = [];
     this.userService.allUsers.forEach(element => {
       const name: string = element['name'];
       const contains: boolean = name.toLocaleLowerCase().indexOf(this.inputs.specificUser.toLocaleLowerCase()) != -1;
-      if (contains) {
-        console.log(element['name']);
-        this.userList.push({ user: name, uid: element['id'] })
+      if (contains && this.inputs.specificUser != '') {
+        this.userList.push({ user: name, uid: element['id'], img: element['profilePicture'] });  
       }
     });
     console.log(this.userList);
   }
 
+
+  focusOnUserInput(){
+    this.specificUserInput?.nativeElement.focus();
+  }
 
   getUsersFromChannel() {
     return
