a966cb15a7d4cc14a37394269c85d2914bd02a1b
forstmeister <c.foerster@tutanota.com>
2024-06-14 12:22:01 +0200

    added functions to close the emoji picker if a user clicks outside the emoji container

--- src/app/mainsection/channel/channel-messages/message/message.component.ts
+++ src/app/mainsection/channel/channel-messages/message/message.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild,HostListener,OnInit,AfterViewInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MainsectionComponent } from '../../../mainsection.component';
 import { AddUserToChannelDialogComponent } from '../../add-user-to-channel-dialog/add-user-to-channel-dialog.component';
@@ -17,10 +17,12 @@ import { EmojiComponent } from '@ctrl/ngx-emoji-mart/ngx-emoji';
   templateUrl: './message.component.html',
   styleUrl: './message.component.scss'
 })
-export class MessageComponent {
+export class MessageComponent  {
   constructor(public dialog: MatDialog, public mainsectionComponent: MainsectionComponent) { }
+   
 
   showEmojiPickerArray: boolean[] = [];
+  isEmojiPickerVisible:boolean = false;
 
   //test Nachrichtaufbau entfällt später, da die Daten von Firebase kommen. 
   messages = [{
@@ -57,9 +59,28 @@ export class MessageComponent {
   ngOnInit() {
     this.showEmojiPickerArray = this.messages.map(() => false);
   }
+
   toggleEmojiPicker(index: number) {
     this.showEmojiPickerArray = this.showEmojiPickerArray.map((value, i) => i === index ? !value : false);
   }
+
+    /**
+ * Toggles the visibility of the emoji picker.
+ * It stops the propagation of the event to prevent it from bubbling up to other event listeners.
+ * It then toggles the `isEmojiPickerVisible` property, which determines whether the emoji picker is visible or not.
+ * @param event - The click event that triggered this function.
+ */
+    toggleEmojiPickerEvent(event: Event) {
+    event.stopPropagation(); 
+    this.isEmojiPickerVisible = !this.isEmojiPickerVisible;
+  }
+
+  /**
+ * Adds an emoji to the selected message.
+ * @param event - The event object that contains the emoji data.
+ * @param index - The index of the message to which the emoji should be added.
+ * @returns {void}
+ */
 
   addEmoji(event: any, index: number) {
     this.messages[index]['selectedEmoji'].push(event['emoji']['native']);
@@ -69,6 +90,7 @@ export class MessageComponent {
     console.log(event['emoji']);
     this.toggleEmojiPicker(index);
     this.emojiCounter();
+   
   }
 
   /**
@@ -96,6 +118,41 @@ export class MessageComponent {
       message.emojiCounts = Array.from(emojiCountsMap, ([emoji, count]) => ({ emoji, count }));
       console.log(message.emojiCounts);
     });
+  }
+
+  /**
+ * Checks if a given element or any of its parent elements have a specific class.
+ * @param element - The element to check.
+ * @param className - The class name to search for.
+ * @returns True if the element or any of its parent elements have the specified class, false otherwise.
+ */
+  isClickedElementOrChildWithClass(element: any, className: string): boolean {
+    while (element) {
+      if (element.classList.contains(className)) {
+        return true;
+      }
+      element = element.parentElement;
+    }
+    return false;
+  }
+
+ @ViewChild('emojiPickerContainer', { static: false }) emojiPickerContainer!: ElementRef;
+
+  /**
+ * Listens for click events on the document and closes the emoji picker if the click is outside of it.
+ * @param event - The click event.
+ */
+ @HostListener('document:click', ['$event'])
+  handleClickOutside(event: Event) {
+    if (!this.isEmojiPickerVisible || !this.emojiPickerContainer) {
+      return; 
+    }
+
+    if (!this.isClickedElementOrChildWithClass(event.target, 'emoji-mart') && this.emojiPickerContainer) {
+      console.log('Clicked outside class "emoji-mart"');
+      this.showEmojiPickerArray = this.messages.map(() => false);
+       this.isEmojiPickerVisible = false; 
+    }
   }
 
   @ViewChild('scroll', { read: ElementRef }) public scroll!: ElementRef<any>;
