98dc1b93ea75936895f2130092b08a92d70c8ba5
Marcel Lakotta <marcel.lakotta@aol.de>
2024-06-21 16:47:43 +0200

    dynamic responsive

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, AfterViewInit, ViewChild, ElementRef, OnInit } from '@angular/core';
 import {
   MatDialog,
   MatDialogRef,
@@ -10,6 +10,7 @@ import {
 import { UserMenuDialogComponent } from './user-menu-dialog/user-menu-dialog.component';
 import { UserService } from '../../services/user.service';
 import { CommonModule } from '@angular/common';
+import { MainsectionComponent } from '../../../mainsection/mainsection.component';
 
 @Component({
   selector: 'app-header',
@@ -18,10 +19,18 @@ import { CommonModule } from '@angular/common';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit, OnInit {
   userService: UserService = inject(UserService);
+  @ViewChild('headlineMobile', { static: true, read: ElementRef }) headlineMobile!: ElementRef;
+  @ViewChild('headlineDesktop', { static: true, read: ElementRef }) headlineDesktop!: ElementRef;
 
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog, private mainsectionComponent: MainsectionComponent) { }
+
+  ngAfterViewInit(): void {
+  }
+
+  ngOnInit(): void {
+  }
 
   /**
    * The `openDialog` function opens a dialog box for the UserMenuDialogComponent at a specific position
@@ -41,5 +50,11 @@ export class HeaderComponent {
   getUserStatus() {
     const loggedIn = this.userService.userInfo.isLoggedIn == true ? "online-div" : "offline-div";
     return loggedIn
+  }
+
+  goBack() {
+    this.mainsectionComponent.showSidenav();
+    this.headlineDesktop.nativeElement.style.display = 'block';
+    this.headlineMobile.nativeElement.style.display = 'none';
   }
 }
