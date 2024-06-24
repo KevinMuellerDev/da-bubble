186b2502b8fbd522d186de71404b06350fe03a62
Kevin Mueller <kevin.mueller@tuta.com>
2024-06-24 11:07:10 +0200

    added msgid to the msg propertys

--- src/app/shared/models/message.class.ts
+++ src/app/shared/models/message.class.ts
@@ -1,28 +1,31 @@
 export class MessageData {
     id: string;
+    msgId: string;
     name: string;
     profilePicture: string;
     message: string;
-    timestamp:number;
-    emoji:[];
+    timestamp: number;
+    emoji: [];
 
     constructor(obj?: any) {
         this.id = obj ? obj.id : "";
         this.name = obj ? obj.name : "";
         this.profilePicture = obj ? obj.profilePicture : "";
         this.message = obj ? obj.message : "";
-        this.timestamp = Date.now()
-        this.emoji = obj ? obj.emoji : []
+        this.timestamp = Date.now();
+        this.emoji = obj ? obj.emoji : [];
+        this.msgId = obj ? obj.msgId : "";
     }
 
-    public toJson(){
+    public toJson() {
         return {
             id: this.id,
             name: this.name,
             profilePicture: this.profilePicture,
             message: this.message,
             timestamp: this.timestamp,
-            emoji: this.emoji	
+            emoji: this.emoji,
+            msgId: this.msgId
         };
     }
 
