c6c8df7a2506fbdaa3ae3371a01696f554b402ed
Kevin Müller <kevin.mueller@tuta.com>
2024-07-03 07:06:07 +0200

    finished search channel in header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -15,19 +15,25 @@ import { MainsectionComponent } from '../../../mainsection/mainsection.component
 import { SidebarComponent } from '../../../mainsection/sidebar/sidebar.component';
 import { SidebarService } from '../../services/sidebar.service';
 import { ChannelService } from '../../services/channel.service';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, MatMenuModule],
+  imports: [CommonModule, MatMenuModule, FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent implements AfterViewInit, OnInit {
+  @ViewChild(MatMenuTrigger) trigger!: MatMenuTrigger;
   userService: UserService = inject(UserService);
   sidebarService: SidebarService = inject(SidebarService);
   channelService: ChannelService = inject(ChannelService);
   userList: any[] = [];
+  messageList: any[] = [];
+  searchMsg = {
+    msg: ''
+  };
   @ViewChild('headlineMobile', { static: true, read: ElementRef }) headlineMobile!: ElementRef;
   @ViewChild('headlineDesktop', { static: true, read: ElementRef }) headlineDesktop!: ElementRef;
 
@@ -59,24 +65,42 @@ export class HeaderComponent implements AfterViewInit, OnInit {
     return loggedIn
   }
 
-/**
- * The `goBack` function shows the side navigation, displays the headline on desktop, and hides the
- * headline on mobile.
- */
+  /**
+   * The `goBack` function shows the side navigation, displays the headline on desktop, and hides the
+   * headline on mobile.
+   */
   goBack() {
     this.mainsectionComponent.showSidenav();
     this.headlineDesktop.nativeElement.style.display = 'block';
     this.headlineMobile.nativeElement.style.display = 'none';
   }
 
-/**
- * The scrollToMessage function scrolls the message container to a specific message based on its index.
- * @param {number} index - The `index` parameter represents the index of the message you 
- * want to scroll to in the message container.
- */
+  /**
+   * The scrollToMessage function scrolls the message container to a specific message based on its index.
+   * @param {number} index - The `index` parameter represents the index of the message you 
+   * want to scroll to in the message container.
+   */
   scrollToMessage(index: number) {
-    const msgElement = document.getElementById('singleMessage-'+index);
+    const msgElement = document.getElementById('singleMessage-' + index);
     const topPos = msgElement!.offsetTop;
     document.getElementById('messageContainer')!.scrollTop = topPos;
+    this.searchMsg.msg = '';
+    this.messageList = [];
+  }
+
+  getMenu() {
+    this.trigger.openMenu();
+  }
+
+  searchUser() {
+    this.messageList = [];
+    this.channelService.messages.forEach((element, i) => {
+      const message: string = element['message'];
+      const contains: boolean = message.toLocaleLowerCase().indexOf(this.searchMsg.msg.toLocaleLowerCase()) != -1;
+      element.index = i;
+      if (contains && this.searchMsg.msg != '') {
+        this.messageList.push(element);
+      }
+    });
   }
 }
