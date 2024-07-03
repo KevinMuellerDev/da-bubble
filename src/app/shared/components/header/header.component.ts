d6b3913af43cbbec8e393b9a2466602620cd727a
Kevin Müller <kevin.mueller@tuta.com>
2024-07-03 13:30:11 +0200

    added scroll smooth behavior in messages component

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -83,7 +83,7 @@ export class HeaderComponent implements AfterViewInit, OnInit {
   scrollToMessage(index: number) {
     const msgElement = document.getElementById('singleMessage-' + index);
     const topPos = msgElement!.offsetTop;
-    document.getElementById('messageContainer')!.scrollTop = topPos;
+    document.getElementById('messageContainer')!.scrollTo({top: topPos, behavior: 'smooth'});
     this.searchMsg.msg = '';
     this.messageList = [];
   }
